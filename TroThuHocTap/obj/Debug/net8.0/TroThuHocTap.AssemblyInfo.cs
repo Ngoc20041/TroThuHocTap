f831252f951fe943dbc2382c4908b1ee1f67392b
Ngoc20041 <buin1905@gmail.com>
2025-03-19 15:59:25 +0700

    Update style

--- TroThuHocTap/obj/Debug/net8.0/TroThuHocTap.AssemblyInfo.cs
+++ TroThuHocTap/obj/Debug/net8.0/TroThuHocTap.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TroThuHocTap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789c4ebcaa3060e890c5725292add67263306738")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2e26a181e5b5ad46862f9f535b5454b25b47be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TroThuHocTap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TroThuHocTap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
